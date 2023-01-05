5ad0131a8e34114cdc7a290fe4242c8798d81ad1
강진우 <wlsdn1252@naver.com>
2023-01-05 16:37:39 +0900

    임시로 방 주소,설명 반영

--- app/src/main/java/com/example/landapp/MainActivity.kt
+++ app/src/main/java/com/example/landapp/MainActivity.kt
@@ -16,13 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mRoomList.add( Room( 24500, "서울시 서대문구", 0, "서울시 서대문구의 반지하 2억 4500만원 방 입니다." ) )
-        mRoomList.add( Room( 8500, "서울시 동작구", 0, "서울시 동작구의 반지하 8500만원 방 입니다." ) )
-        mRoomList.add( Room( 4500, "서울시 은평구", -2, "서울시 은평구의 지하 2층 4500만원 방 입니다." ) )
-        mRoomList.add( Room( 182500, "서울시 중랑구", 5, "서울시 중랑구의 5층 1억 18억 2500만원 방 입니다." ) )
-        mRoomList.add( Room( 235000, "서울시 도봉구", 7, "서울시 도봉구의 7층 1억 23억 5000만원 방 입니다." ) )
-        mRoomList.add( Room( 24000, "서울시 강남구", 19, "서울시 강남구의 19층 2억 4000만원 방 입니다." ) )
-        mRoomList.add( Room( 3700, "서울시 서초구", -1, "서울시 서초구의 지하 1층 3700만원 방 입니다." ) )
+        mRoomList.add( Room( 24500, "서울시 서대문구", 0, "서대문구의 반지하 2억 4500만원 방 입니다." ) )
+        mRoomList.add( Room( 8500, "서울시 동작구", 0, " 동작구의 반지하 8500만원 방 입니다." ) )
+        mRoomList.add( Room( 4500, "서울시 은평구", -2, " 은평구의 지하 2층 4500만원 방 입니다." ) )
+        mRoomList.add( Room( 182500, "서울시 중랑구", 5, " 중랑구의 5층 1억 18억 2500만원 방 입니다." ) )
+        mRoomList.add( Room( 235000, "서울시 도봉구", 7, " 도봉구의 7층 1억 23억 5000만원 방 입니다." ) )
+        mRoomList.add( Room( 24000, "서울시 강남구", 19, " 강남구의 19층 2억 4000만원 방 입니다." ) )
+        mRoomList.add( Room( 3700, "서울시 서초구", -1, " 서초구의 지하 1층 3700만원 방 입니다." ) )
 
         mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRoomList)
         roomListView.adapter = mRoomAdapter
--- app/src/main/java/com/example/landapp/adapters/RoomAdapter.kt
+++ app/src/main/java/com/example/landapp/adapters/RoomAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.landapp.R
 import com.example.landapp.datas.Room
 
@@ -22,6 +23,15 @@ class RoomAdapter(
             tempRow = inflater.inflate(R.layout.room_list_item, null)
         }
          val row = tempRow!!
+
+        val roomData = mList[position]
+
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
+        val descriprionTxt = row.findViewById<TextView>(R.id.descriprionTxt)
+
+        descriprionTxt.text = roomData.description
+        addressAndFloorTxt.text = roomData.address
 
         return row
 
