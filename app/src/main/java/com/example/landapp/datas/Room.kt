765f021a8d40e43c39181f20f2b78d3c4173a91d
강진우 <wlsdn1252@naver.com>
2023-01-05 16:43:25 +0900

    방의 기능으로 층수 가공 String으로 주는 기능 추가

--- app/src/main/java/com/example/landapp/datas/Room.kt
+++ app/src/main/java/com/example/landapp/datas/Room.kt
@@ -2,4 +2,15 @@ package com.example.landapp.datas
 
 // 리스트 한줄에 표현할 하위정보, 가격,주소,층수,설명
 class Room(val price : Int, val address : String, val floor : Int, val description : String) {
+    
+    fun getFormattedFloor() : String{
+        
+        if(this.floor >= 1){
+            return "${this.floor}층"
+        }else if(this.floor == 0){
+            return "반지하"
+        }else{
+            return "지하${-this.floor}층"
+        }
+    }
 }
