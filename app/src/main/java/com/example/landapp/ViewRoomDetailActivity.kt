ece54aef4d57d5f3ad9faac5efba5b9de0da3f8d
강진우 <wlsdn1252@naver.com>
2023-01-11 15:51:14 +0900

    방 상세 정보 화면 UI추가

---
+++ app/src/main/java/com/example/landapp/ViewRoomDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.landapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRoomDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detail)
+    }
+}
